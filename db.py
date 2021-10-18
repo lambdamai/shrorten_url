084b7a0ba55b5bf375343cacb276b6aab82cf668
mr8bit <a.mazaew@lambda-it.ru>
2021-10-18 20:16:42 +0300

    Добавили ссылку на DB. Закрываем #1

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
+from os import environ
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = environ.get("DB_URL", "sqlite:///./sql_app.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
